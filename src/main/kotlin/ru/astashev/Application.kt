c1dd669970b174559d33ee34750cce510e721ed6
vastashev <slavik_82.09@mail.ru>
2023-09-11 00:20:27 +0300

    Lesson 1

--- src/main/kotlin/ru/astashev/Application.kt
+++ src/main/kotlin/ru/astashev/Application.kt
@@ -3,7 +3,7 @@ package ru.astashev
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import ru.astashev.plugins.*
+import ru.astashev.plugins.DatabaseFactory.initializationDatabase
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -11,9 +11,9 @@ fun main() {
 }
 
 fun Application.module() {
-    configureSerialization()
-    configureDatabases()
-    configureMonitoring()
-    configureSecurity()
-    configureRouting()
+    initializationDatabase()
+//    configureSecurity()
+//    configureMonitoring()
+//    configureSerialization()
+//    configureRouting()
 }
